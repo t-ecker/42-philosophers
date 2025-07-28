44ded8b4fa8409ff287588e0cf391f56e35bf1ac
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-07-28 21:22:34 +0200

    simplify observer functions and adding semaphores

---
+++ philo_bonus/observer.c
@@ -0,0 +1,27 @@
+#include "./philo.h"
+
+bool	check_meals_eaten(t_philo *philo)
+{
+	sem_wait(philo->eating);
+	bool finishedEating = (philo->meal_count >= philo->data->max_meals) && philo->data->max_meals > 0;
+	sem_post(philo->eating);
+	return (finishedEating);
+}
+
+void	*checkDeath(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	while (1)
+	{
+		sem_wait(philo->eating);
+		if (current_time_in_ms() - philo->last_meal > philo->data->time_to_die)
+		{
+			write_message("died", philo->data, philo->num, true);
+			exit(EXIT_DIED);
+		}
+		sem_post(philo->eating);
+		// usleep(100);
+	}
+}
