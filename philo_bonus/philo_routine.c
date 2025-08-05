ec53432cf0510e9cee6139b5f50a26b0e56ac6e6
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-08-05 19:59:02 +0200

    add routine thread to child process to make terminating better + better cleanup

--- philo_bonus/philo_routine.c
+++ philo_bonus/philo_routine.c
@@ -1,25 +1,37 @@
 #include "./philo.h"
 
-void terminate(t_philo *philo)
+void terminate(t_philo *philo, t_exit_code exitCode)
 {
-	// sem_close(philo->data->forks);
-	// sem_close(philo->data->access);
-	// sem_close(philo->data->status_mutex);
-	// sem_close(philo->data->terminate);
+	// cleanup(philo->data);
+	sem_close(philo->data->forks);
+	sem_close(philo->data->write);
+	sem_close(philo->data->access);
+	sem_close(philo->data->terminate);
+	sem_close(philo->data->mainStop_lock);
+	sem_close(philo->data->max_meals_sem);
+
+	if (philo->checkOwnDeath_Thread)
+		pthread_join(philo->checkOwnDeath_Thread, NULL);
+	if (philo->routine_Thread)
+		pthread_join(philo->routine_Thread, NULL);
 	
 	if (philo->semName_eating)
 	{
 		sem_close(philo->eating_lock);
 		sem_unlink(philo->semName_eating);
 		free(philo->semName_eating);
+		philo->semName_eating = NULL;
 	}
 	if (philo->semName_shutdown)
 	{
 		sem_close(philo->shutdown_lock);
 		sem_unlink(philo->semName_shutdown);
 		free(philo->semName_shutdown);
+		philo->semName_shutdown = NULL;
 	}
-	exit(philo->statusCode);
+	if (philo->statusCode == EXIT_DIED || philo->statusCode == EXIT_MAX_MEALS)
+		exitCode = philo->statusCode;
+	exit(exitCode);
 }
 
 void	routine(t_philo *philo)
@@ -39,12 +51,21 @@ void	routine(t_philo *philo)
 	sem_post(philo->data->forks);
 	sem_post(philo->data->forks);
 	sem_post(philo->data->access);
-	if (check_meals_eaten(philo))
-		terminate(philo);
+	check_meals_eaten(philo);
 	if (check_shutdown(philo))
-		terminate(philo);
+		return ;
 	write_message("is sleeping", philo->data, philo->num, false);
 	ft_usleep(philo->data->time_to_sleep);
+}
+
+void *routine_Thread(void *args)
+{
+	t_philo *philo;
+
+	philo = args;
+	while (!check_shutdown(philo))
+		routine(philo);
+	return NULL;
 }
 
 int init_philo(t_data *data, t_philo *philo)
@@ -62,7 +83,6 @@ int init_philo(t_data *data, t_philo *philo)
 	if (philo->eating_lock == SEM_FAILED || philo->shutdown_lock == SEM_FAILED)
 		return (1);
 	philo->data = data;
-	philo->statusCode = EXIT_ERROR;
 	philo->last_meal = current_time_in_ms();
 	philo->data->start_time = current_time_in_ms();
 	
@@ -78,17 +98,22 @@ void	philo_routine(void *arg)
 	if (init_philo(data, &philo))
 	{
 		philo.data = data;
-		terminate(&philo);
+		terminate(&philo, EXIT_ERROR);
 	}
 	if (pthread_create(&philo.checkOwnDeath_Thread, NULL, checkOwnDeath, &philo) != 0)
-		terminate(&philo);
-	if (pthread_create(&philo.checkOthersDeath_Thread, NULL, checkOthersDeath, &philo) != 0)
-		terminate(&philo);
-	if (pthread_detach(philo.checkOthersDeath_Thread) != 0
-		|| pthread_detach(philo.checkOwnDeath_Thread) != 0)
-		terminate(&philo);
-	philo.statusCode = EXIT_SUCCESSFUL;
-	while (!check_shutdown(&philo))
-		routine(&philo);
-	terminate(&philo);
+		terminate(&philo, EXIT_ERROR);
+	if (pthread_create(&philo.routine_Thread, NULL, routine_Thread, &philo) != 0)
+	{
+		sem_wait(philo.shutdown_lock);
+		philo.shutdown = true;
+		sem_post(philo.shutdown_lock);
+		pthread_join(philo.checkOwnDeath_Thread, NULL);
+		philo.checkOwnDeath_Thread = 0;
+		terminate(&philo, EXIT_ERROR);
+	}
+	sem_wait(data->terminate);
+	sem_wait(philo.shutdown_lock);
+	philo.shutdown = true;
+	sem_post(philo.shutdown_lock);
+	terminate(&philo, EXIT_CLOSED);
 }
