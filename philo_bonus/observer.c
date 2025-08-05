dec9ebaa43b5b8fa34d5f4b5b76c70aa36278c66
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-08-05 20:00:16 +0200

    observe monitoring functions to fit new terminating logic

--- philo_bonus/observer.c
+++ philo_bonus/observer.c
@@ -1,24 +1,56 @@
 #include "./philo.h"
 
-bool	check_meals_eaten(t_philo *philo)
+void	check_meals_eaten(t_philo *philo)
 {
 	sem_wait(philo->eating_lock);
-	bool finishedEating = philo->data->max_meals > 0 && (philo->meal_count >= philo->data->max_meals);
-	sem_post(philo->eating_lock);
-	if (finishedEating)
+	if (philo->data->max_meals > 0 && (philo->meal_count >= philo->data->max_meals) && !philo->hasFinished)
 	{
+		sem_post(philo->data->max_meals_sem);
 		sem_wait(philo->shutdown_lock);
 		philo->shutdown = true;
+		philo->hasFinished = true;
 		philo->statusCode = EXIT_MAX_MEALS;
 		sem_post(philo->shutdown_lock);
 	}
-	return (finishedEating);
+	sem_post(philo->eating_lock);
+}
+
+void *check_all_meals_eaten(void *args)
+{
+	t_data *data;
+	int finishedEating;
+	int i;
+	bool res;
+
+	i = -1;
+	finishedEating = 0;
+	data = args;
+	while(++i < data->philo_count)
+	{
+		sem_wait(data->max_meals_sem);
+
+		sem_wait(data->mainStop_lock);
+		res = data->mainStop;
+		sem_post(data->mainStop_lock);
+		if (res)
+			return NULL;
+		if (++finishedEating >= data->philo_count)
+		{
+			i = -1;
+			while (++i < data->philo_count)
+				sem_post(data->terminate);
+			break;
+		}
+	}
+	return NULL;
 }
 
 void	*checkOwnDeath(void *arg)
 {
 	t_philo	*philo;
+	int i;
 
+	i = -1;
 	philo = (t_philo *)arg;
 	while (!check_shutdown(philo))
 	{
@@ -29,6 +61,8 @@ void	*checkOwnDeath(void *arg)
 			sem_wait(philo->shutdown_lock);
 			philo->shutdown = true;
 			philo->statusCode = EXIT_DIED;
+			while(++i < philo->data->philo_count)
+				sem_post(philo->data->terminate);
 			sem_post(philo->shutdown_lock);
 			sem_post(philo->eating_lock);
 			break ;
@@ -36,18 +70,6 @@ void	*checkOwnDeath(void *arg)
 		sem_post(philo->eating_lock);
 		usleep(philo->data->philo_count * 50);
 	}
-	return NULL;
-}
-
-void	*checkOthersDeath(void *arg)
-{
-	t_philo	*philo;
-
-	philo = (t_philo *)arg;
-	sem_wait(philo->data->terminate);
-	sem_wait(philo->shutdown_lock);
-	philo->shutdown = true;
-	sem_post(philo->shutdown_lock);
 	return NULL;
 }
 
